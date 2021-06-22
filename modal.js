48988fd6b644cf9ad6c37d74175c5ea14f155b6a
Miguel Paredes Julien <julien314@hotmail.fr>
2021-06-22 18:17:22 +0200

    Ajout de la fonctionnalité Fermer au bouton (X) + réorganisation des fichiers

--- modal.js
+++ modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModalBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,9 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Close Modal 
+
+closeModalBtn.addEventListener("click", function () {
+  modalbg.style.display = "none";
+});
 
